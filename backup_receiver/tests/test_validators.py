ea630fa67c2c7ae23d9f6b03421ef0f28056d486
drz <me@drz.se>
2024-08-02 09:13:10 +0200

    Add test for validator of filename string

--- backup_receiver/tests/test_validators.py
+++ backup_receiver/tests/test_validators.py
@@ -1,9 +1,29 @@
 from flask import current_app
-from backup_receiver.validators import is_password_allowed, is_sha256_allowed
+from backup_receiver.validators import is_password_allowed, is_sha256_allowed, is_filename_allowed
 import pytest
 import os
 
+def test_is_filename_allowed():
+    assert is_filename_allowed("myfilename-2024_08_08.zip") == True
+    assert is_filename_allowed("myfilename-_.2024_08_08.zip") == True
+    assert is_filename_allowed(".myfilename.zip") == False
+    assert is_filename_allowed("_myfilename.zip") == False
+    assert is_filename_allowed("-myfilename.zip") == False
+    assert is_filename_allowed("myfilename.zip.") == False
+    assert is_filename_allowed("myfilename.zip_") == False
+    assert is_filename_allowed("myfilename.zip-") == False
+    assert is_filename_allowed("myfilename..zip") == False
+    assert is_filename_allowed("myfilenam__e.zip") == False
+    assert is_filename_allowed("myfilenam--e.zip") == False
+    assert is_filename_allowed("myfilenam,e.zip") == False
+    assert is_filename_allowed("myfilenam\"e.zip") == False
+    assert is_filename_allowed("myfilenam\'e.zip") == False
+    assert is_filename_allowed("../myfilename.zip") == False
+    assert is_filename_allowed("") == False
+
 def test_is_sha256_allowed():
+    assert is_sha256_allowed("7b7632005be0f36c5d1663a6c5ec4d13315589d65e1ef8687fb4b9866f9bc4b0") == True
+    assert is_sha256_allowed("") == False
     assert is_sha256_allowed("a1d4") == False
     assert is_sha256_allowed("a1b2") == False
     assert is_sha256_allowed("7b7632005be0f36c5d1663a6c5ec4d13315589d65e1ef8687fb4b9866f9bc4b0a") == False
@@ -12,10 +32,10 @@ def test_is_sha256_allowed():
     assert is_sha256_allowed("7b7632005be\"f36c5d1663a6c5ec4d13315589d65e1ef8687fb4b9866f9bc4b0") == False
     assert is_sha256_allowed("7b7632005be-f36c5d1663a6c5ec4d13315589d65e1ef8687fb4b9866f9bc4b0") == False
     assert is_sha256_allowed("7b7632005b.0f36c5d1663a6c5ec4d13315589d65e1ef8687fb4b9866f9bc4b0") == False
-    assert is_sha256_allowed("7b7632005be0f36c5d1663a6c5ec4d13315589d65e1ef8687fb4b9866f9bc4b0") == True
 
 def test_is_password_allowed():
-    assert is_password_allowed("aA8/+=") == True
+    assert is_password_allowed("aA8/+=ab3cfF5ds6aD") == True
+    assert is_password_allowed("") == False
     assert is_password_allowed("aA8/+=\\") == False
     assert is_password_allowed("aA8/+=\\vfgg") == False
     assert is_password_allowed("aAx\"fds") == False
