38c87bcd4246bb29f6c8bb7d7c3954fc108ff291
drz <me@drz.se>
2024-08-04 16:11:32 +0200

    Remove debug print

--- backup_receiver/tests/test_application.py
+++ backup_receiver/tests/test_application.py
@@ -4,7 +4,7 @@ from io import BytesIO
 def test_sha256_of_file():
     test_file_path = "backup_receiver/tests/test_file.txt"
     test_file_sha256 = "7b7632005be0f36c5d1663a6c5ec4d13315589d65e1ef8687fb4b9866f9bc4b0"
-    
+
     assert sha256_of_file(test_file_path) == test_file_sha256
 
 def test_receive_backup_no_password(client):
@@ -12,8 +12,6 @@ def test_receive_backup_no_password(client):
     test_file_path = "backup_receiver/tests/test_file.txt"
     f = open(test_file_path, "r")
     test_file_data = f.read()
-
-    print("data:"  + str(test_file_data))
 
     response = client.post("/receive_backup", buffered=True, content_type='multipart/form-data', data={ "filename":"test_file.txt", "file":(BytesIO(bytes(test_file_data, 'utf-8')), "test_file.txt"), "sha256":test_file_sha256})
 
