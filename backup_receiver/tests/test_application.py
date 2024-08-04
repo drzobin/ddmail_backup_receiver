63557956402b75e7805c39d87ec70104a9989d89
drz <me@drz.se>
2024-08-04 16:28:56 +0200

    Fix formatting

--- backup_receiver/tests/test_application.py
+++ backup_receiver/tests/test_application.py
@@ -15,11 +15,16 @@ def test_receive_backup_no_password(client):
     f = open(test_file_path, "r")
     test_file_data = f.read()
 
-    response = client.post("/receive_backup", buffered=True, content_type='multipart/form-data', data={
-        "filename": "test_file.txt",
-        "file": (BytesIO(bytes(test_file_data, 'utf-8')), "test_file.txt"),
-        "sha256": test_file_sha256
-    })
+    response = client.post(
+        "/receive_backup",
+        buffered=True,
+        content_type='multipart/form-data',
+        data={
+            "filename": "test_file.txt",
+            "file": (BytesIO(bytes(test_file_data, 'utf-8')), "test_file.txt"),
+            "sha256": test_file_sha256
+            }
+        )
 
     assert response.status_code == 200
     assert b"error: password is none" in response.data
@@ -31,12 +36,17 @@ def test_receive_backup_password_illigal_char(client):
     f = open(test_file_path, "r")
     test_file_data = f.read()
 
-    response = client.post("/receive_backup", buffered=True, content_type='multipart/form-data', data={
-        "password": "password$",
-        "filename": "test_file.txt",
-        "file": (BytesIO(bytes(test_file_data, 'utf-8')), "test_file.txt"),
-        "sha256": test_file_sha256
-    })
+    response = client.post(
+        "/receive_backup",
+        buffered=True,
+        content_type='multipart/form-data',
+        data={
+            "password": "password$",
+            "filename": "test_file.txt",
+            "file": (BytesIO(bytes(test_file_data, 'utf-8')), "test_file.txt"),
+            "sha256": test_file_sha256
+            }
+        )
 
     assert response.status_code == 200
     assert b"error: password validation failed" in response.data
