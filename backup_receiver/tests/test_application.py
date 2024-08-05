0b8b7cb5f1a3aff0b99b9a1be044d907e7fd1eee
drz <me@drz.se>
2024-08-05 08:06:46 +0200

    Add test for filename string

--- backup_receiver/tests/test_application.py
+++ backup_receiver/tests/test_application.py
@@ -1,20 +1,18 @@
 from backup_receiver.application import sha256_of_file
 from io import BytesIO
 
+test_file_path = "backup_receiver/tests/test_file.txt"
+sha256 = "7b7632005be0f36c5d1663a6c5ec4d13315589d65e1ef8687fb4b9866f9bc4b0"
+
+f = open(test_file_path, "r")
+test_file_data = f.read()
+
 
 def test_sha256_of_file():
-    test_file_path = "backup_receiver/tests/test_file.txt"
-    test_file_sha256 = "7b7632005be0f36c5d1663a6c5ec4d13315589d65e1ef8687fb4b9866f9bc4b0"
-
-    assert sha256_of_file(test_file_path) == test_file_sha256
+    assert sha256_of_file(test_file_path) == sha256
 
 
 def test_receive_backup_no_password(client):
-    test_file_sha256 = "7b7632005be0f36c5d1663a6c5ec4d13315589d65e1ef8687fb4b9866f9bc4b0"
-    test_file_path = "backup_receiver/tests/test_file.txt"
-    f = open(test_file_path, "r")
-    test_file_data = f.read()
-
     response = client.post(
         "/receive_backup",
         buffered=True,
@@ -22,7 +20,7 @@ def test_receive_backup_no_password(client):
         data={
             "filename": "test_file.txt",
             "file": (BytesIO(bytes(test_file_data, 'utf-8')), "test_file.txt"),
-            "sha256": test_file_sha256
+            "sha256": sha256
             }
         )
 
@@ -31,11 +29,6 @@ def test_receive_backup_no_password(client):
 
 
 def test_receive_backup_password_illigal_char(client):
-    test_file_sha256 = "7b7632005be0f36c5d1663a6c5ec4d13315589d65e1ef8687fb4b9866f9bc4b0"
-    test_file_path = "backup_receiver/tests/test_file.txt"
-    f = open(test_file_path, "r")
-    test_file_data = f.read()
-
     response = client.post(
         "/receive_backup",
         buffered=True,
@@ -44,9 +37,42 @@ def test_receive_backup_password_illigal_char(client):
             "password": "password$",
             "filename": "test_file.txt",
             "file": (BytesIO(bytes(test_file_data, 'utf-8')), "test_file.txt"),
-            "sha256": test_file_sha256
+            "sha256": sha256
             }
         )
 
     assert response.status_code == 200
     assert b"error: password validation failed" in response.data
+
+
+def test_receive_backup_no_filename(client):
+    response = client.post(
+        "/receive_backup",
+        buffered=True,
+        content_type='multipart/form-data',
+        data={
+            "password": "password",
+            "file": (BytesIO(bytes(test_file_data, 'utf-8')), "test_file.txt"),
+            "sha256": sha256
+            }
+        )
+
+    assert response.status_code == 200
+    assert b"error: filename is none" in response.data
+
+
+def test_receive_backup_filename_illigal_char(client):
+    response = client.post(
+        "/receive_backup",
+        buffered=True,
+        content_type='multipart/form-data',
+        data={
+            "password": "password",
+            "filename": "test_fil--e.txt",
+            "file": (BytesIO(bytes(test_file_data, 'utf-8')), "test_file.txt"),
+            "sha256": sha256
+            }
+        )
+
+    assert response.status_code == 200
+    assert b"error: filename validation failed" in response.data
