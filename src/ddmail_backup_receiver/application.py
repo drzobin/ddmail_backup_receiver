3710185e715321a004e99aa44c03171db5052224
change_me <change_me>
2025-04-16 09:01:24 +0200

    refactor to use logger

--- src/ddmail_backup_receiver/application.py
+++ src/ddmail_backup_receiver/application.py
@@ -1,17 +1,13 @@
+import os
+import time
+import hashlib
 from flask import Blueprint, current_app, request
 from argon2 import PasswordHasher
 from werkzeug.utils import secure_filename
-import os
-import time
-import logging
-import hashlib
 
 from backup_receiver.validators import is_domain_allowed, is_password_allowed, is_email_allowed, is_sha256_allowed, is_filename_allowed
 
 bp = Blueprint("application", __name__, url_prefix="/")
-
-# Configure logging.
-logging.basicConfig(filename="/var/log/ddmail_backup_receiver.log", format='%(asctime)s: %(levelname)s: %(message)s', level=logging.ERROR)
 
 # Get sha256 checksum of file.
 def sha256_of_file(file):
@@ -33,7 +29,7 @@ def sha256_of_file(file):
 def receive_backup():
         # Check if post data contains file.
         if 'file' not in request.files:
-            logging.error("receive_backup() file is not in request.files")
+            current_app.logger.error("file is not in request.files")
             return "error: file is not in request.files"
 
         # Get post data.
@@ -44,22 +40,22 @@ def receive_backup():
 
         # Check if file is None.
         if file == None:
-            logging.error("receive_backup() file is None")
+            current_app.logger.error("file is None")
             return "error: file is none"
 
         # Check if filename is None.
         if filename == None:
-            logging.error("receive_backup() filename is None")
+            current_app.logger.error("filename is None")
             return "error: filename is none"
 
         # Check if password is None.
         if password == None:
-            logging.error("receive_backup() password is None")
+            current_app.logger.error("receive_backup() password is None")
             return "error: password is none"
 
         # Check if sha256 checksum is None.
         if sha256_from_form == None:
-            logging.error("receive_backup() sha256_from_form is None")
+            current_app.logger.error("receive_backup() sha256_from_form is None")
             return "error: sha256_from_form is none"
 
         # Remove whitespace character.
@@ -69,17 +65,17 @@ def receive_backup():
 
         # Validate filename.
         if is_filename_allowed(filename) != True:
-            logging.error("receive_backup() filename validation failed")
+            current_app.logger.error("filename validation failed")
             return "error: filename validation failed"
 
         # Validate sha256 from form.
         if is_sha256_allowed(sha256_from_form) != True:
-            logging.error("receive_backup() sha256 checksum validation failed")
+            current_app.logger.error("sha256 checksum validation failed")
             return "error: sha256 checksum validation failed"
 
         # Validate password.
         if is_password_allowed(password) != True:
-            logging.error("receive_backup() password validation failed")
+            current_app.logger.error("password validation failed")
             return "error: password validation failed"
 
         # Check if password is correct.
@@ -87,11 +83,11 @@ def receive_backup():
         try:
             if ph.verify(current_app.config["PASSWORD_HASH"], password) != True:
                 time.sleep(1)
-                logging.error("receive_backup() wrong password")
+                current_app.logger.error("wrong password")
                 return "error: wrong password1"
         except:
             time.sleep(1)
-            logging.error("receive_backup() wrong password")
+            current_app.logger.error("wrong password")
             return "error: wrong password2"
         time.sleep(1)
 
@@ -100,7 +96,7 @@ def receive_backup():
 
         # Check if upload folder exist.
         if os.path.isdir(upload_folder) != True:
-            logging.error("receive_backup() upload folder " + upload_folder + " do not exist")
+            current_app.logger.error("upload folder " + upload_folder + " do not exist")
             return "error: upload folder " + upload_folder  + " do not exist"
 
         # Save file to disc.
@@ -110,8 +106,8 @@ def receive_backup():
         # Take sha256 checksum of file and compare with checksum from form.
         sha256_from_file = sha256_of_file(full_path)
         if sha256_from_form != sha256_from_file:
-            logging.error("receive_backup() sha256 checksum do not match")
+            current_app.logger.error("sha256 checksum do not match")
             return "error: sha256 checksum do not match"
 
-        logging.info("receive_backup() done")
+        current_app.logger.info("done")
         return "done"
